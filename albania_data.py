a788f48c7ee91b1123fc1abb9254702362097766
Puneet Bhaskar <kvppss@gmail.com>
2025-03-06 23:44:20 -0800

    Scraping completed

--- albania_data.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_spss("raw_data/Modul_1A_householdroster.sav")
-print(df.head())
---
+++ albania_scraper.py
@@ -0,0 +1,129 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from io import StringIO
+from tqdm import tqdm
+
+def id_label(var_list):
+    if 'hhid' in var_list:
+        id_flag = 0
+
+    elif 'psu' in var_list and 'hh' in var_list:
+        id_flag = 1
+        if 'id' in var_list or 'idcode' in var_list:
+            id_flag = 2
+        if 'hh_key' in var_list:
+            id_flag = 3
+        
+    else:
+        id_flag = -1
+
+    return id_flag
+
+
+# Albania URL
+URL = "https://microdata.worldbank.org/index.php/catalog/1970/data-dictionary"
+
+print("Extracting module names and descriptions...")
+response = requests.get(URL)
+df_list = pd.read_html(response.content) #Extracts all tables on page as a single df
+df = df_list[0] # Selects only df in list as df
+
+# Descriptive information in first column "Data file", string indexing to get module name and description
+# Store each module as row in main_df
+df["module_name"] = df["Data file"].apply(lambda x: x.split(" ")[0]) 
+#df["module_description"] = df["Data file"].apply(lambda x: (" ").join(x.split("_")[1:]))
+df["module_description"] = df["Data file"].apply(lambda x: (" ").join(x.split(" ")[1:]))
+df.drop(columns=["Data file"], inplace=True)
+main_df = df.copy() 
+
+
+
+all_dics = []
+print("Extracting variable names, descriptions, and types...")
+for i in range(len(main_df)):
+#for i in range(1):
+    print("Extracting from module {}...".format(main_df["module_name"][i]))
+
+    #Extract information about each module from main_df
+    module = main_df["module_name"][i] 
+    module_description = main_df["module_description"][i]
+
+    #Edge case for module 17 onwards, url naming changed
+    if i + 1 > 30:
+        i += 1
+    #Navigate to data site for each module and capture it as beautifulsoup object 
+    module_url = URL + f"/F{i + 1}?file_name={module}"
+    response = requests.get(module_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Search for containers that have the info we want -- which is variable name and link to variable webpage
+    var_ids = soup.find_all("a", class_="var-id text-break")
+    list_of_dics = []    
+    var_list = [] # Create a list to capture variables from module
+
+    for var_id in var_ids:
+        list_of_dics.append({"variable_name": var_id.text.lower(), "variable_link": var_id["href"]})
+        var_list.append(var_id.text.lower())
+
+    #Handling edge cases for Module 10 with misnamed variable
+    if module == 'Modul_10_Fertility':
+        var_list = ['psu' if x == 'm0_q00' else x for x in var_list]
+        var_list = ['hh' if x == 'm0_q01' else x for x in var_list]
+
+
+    """ 
+    Create an id flag,
+    0 = household identifier 'hhid' present, proxy household 'psu','hh' present, individual id present
+    1 = proxy household id present, individual id absent
+    2 = 'hhid' absent, proxy household 'psu', 'hh' present, individual id present
+    3 = Mapping modules, only household ids present
+    """    
+    id_flag = id_label(var_list)
+    id_mapping = {
+        0: 'hhid',
+        1: 'psu-hh',
+        2: 'id',
+        3: 'hh_key'
+    }
+    unique_id = id_mapping.get(int(id_flag))
+    rem_id = []
+     
+    # For every variable in the module, extract variable description and variable type by going to the variable webpage
+    for j in tqdm(range(len(list_of_dics))):
+    #for j in tqdm(range(2)):
+        # Select dictionary associated with module
+        dic = list_of_dics[j] 
+ 
+        if dic["variable_name"] in ["psu","hh","idcode", "id", "hhid"]:
+            del dic["variable_link"]
+            rem_id.append(j)
+            continue
+
+        # Find data by parsing through link for each variable and creating beautiful soup object
+        var_link = dic["variable_link"]
+        var_response = requests.get(var_link)
+        var_soup = BeautifulSoup(var_response.content, 'html.parser')
+        var_data = var_soup.find_all("div", class_="variable-container")
+
+        # Fill up output dictionary based on data
+        del dic["variable_link"]
+        var_description_raw = var_data[0].find("h2").text.strip()
+        var_description = (" ").join(var_description_raw.split(" ")[:-1])
+        var_type = var_data[0].find("div", class_="fld-inline sum-stat sum-stat-var_intrvl").text.split(":")[1].strip()
+        dic["variable_description"] = var_description
+        dic["module_name"] = module
+        dic["module_description"] = module_description
+        dic["data_type"] = var_type
+        dic["unique_id"] = unique_id
+
+    pruned_list_of_dics = []
+    for k, dic in enumerate(list_of_dics):
+        if k not in rem_id:
+            pruned_list_of_dics.append(dic)
+
+    all_dics += pruned_list_of_dics
+
+pd.DataFrame(all_dics).to_csv("output/albania_metadata.csv", index=False)
+
+
